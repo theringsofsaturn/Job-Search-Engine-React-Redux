87336b6d4688ffa2c2aeab8a63f891f6b0b9cd6d
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-29 19:37:47 +0100

    Register page updated and fixed

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
-import {configureStore} from './redux/store'
+import {configureStore, persistor} from './redux/store'
+import {PersistGate} from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <React.StrictMode>
    <Provider store={configureStore}>
+     <PersistGate loading={null} persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
